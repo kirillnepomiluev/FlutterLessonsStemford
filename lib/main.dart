3293d04035a90a0c25e5cd1e9b63606f5f2e8dd2
Kirill Nepomiluev <nepomiluev.kirill@gmail.com>
2021-10-15 00:23:34 +0300

    Урок 26

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,190 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Lessons());
 
+/// Сегодня мы познакомимся с созданием и оформлением всплывающего меню.
+
 class Lessons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Уроки Flutter",
       home: Scaffold(
+
+        drawer: Drawer( /// Виджет Drawer() позволяет нам создать боковое меню
+          /// страницы
+
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border(
+                right: BorderSide(
+                  color: Colors.blueAccent,
+                  width: 2,
+                )
+              ),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.blueAccent,
+                  Colors.lightBlueAccent,
+                  Colors.white,
+                  Colors.lightBlueAccent,
+                  Colors.blueAccent,
+                ],
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: 100,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.blueAccent
+                      ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          child: Icon(Icons.account_box, size: 30, color: Colors.lightBlueAccent,),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5),
+                          child: Text(
+                            "Профиль",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              color: Colors.lightBlueAccent
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 2,
+                          color: Colors.blueAccent
+                      ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          child: Icon(Icons.edit, size: 30, color: Colors.lightBlueAccent,),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5),
+                          child: Text(
+                            "Заметки",
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.lightBlueAccent
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 2,
+                          color: Colors.blueAccent
+                      ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          child: Icon(Icons.timer, size: 30, color: Colors.lightBlueAccent,),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5),
+                          child: Text(
+                            "Уведомления",
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.lightBlueAccent
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 2,
+                          color: Colors.blueAccent
+                      ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                          child: Icon(Icons.settings, size: 30, color: Colors.lightBlueAccent,),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5),
+                          child: Text(
+                            "Настройки",
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.lightBlueAccent
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
         appBar: AppBar(
           centerTitle: true,
           title: Text(
-            "Урок 22",
+            "Урок 26",
           ),
           backgroundColor: Colors.blueAccent,
         ),
@@ -23,50 +197,6 @@ class Lessons extends StatelessWidget {
             BottomNavigationBarItem(icon: Icon(Icons.search), label: "Поиск"),
           ],
         ),
-
-        /// На данном уроке мы научимся:
-        /// 1) Пользоваться виджетом Icon(), и его свойствами.
-        /// 2) Добавлять собственные иконки.
-        /// 3) Добавлять в приложение изображение из интернета.
-        /// 4) Добавлять в приложение изображение из директории приложения.
-
-        body: Align(
-          alignment: Alignment.center,
-          child: Icon( /// Виджет, позволяющий добавить в приложение иконки.
-
-
-              Icons.remove_red_eye_outlined, /// Конструктор Icons. аналогично
-            /// конструктору Colors. позволяет выбирать иконки из библиотеки
-            /// Flutter.
-
-              size: 70, // Свойство, задающее размер иконки.
-
-              color: Colors.lightBlueAccent, // Свойство, задающее цвет иконки.
-
-          ),
-        ),
-
-        /// Теперь рассмотрим пример, в котором научимся добавлять в приложение
-        /// изображение из интернета.
-        
-        // body: Center( // Синтаксический сахар, иная форма записи виджета Align()
-        //   // со значением Alignment.center.
-        //
-        //   child: Container(
-        //     width: 200,
-        //     height: 400,
-        //     child: Image.network("https://cdn1.ozone.ru/multimedia/1037547238.jpg",
-        //       /// Конструктор Image.network позволяет добавить в приложение
-        //       /// изображение из интернета. Для этого, передаем в конструктор ссылку
-        //       /// на изображение, указанную в ковычках.
-        //
-        //       fit: BoxFit.contain, /// Свойство, отвечающее за размещение
-        //       /// изображения внутри родительского виджета, его размеры и
-        //       /// пропорции. Принимает в себя конструкторы BoxFit.
-        //
-        //     ),
-        //   ),
-        // ),
       ),
     );
   }
