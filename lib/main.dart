b28909d299f59a96c4052c182bfc9e0b0d6d4cf1
Kirill Nepomiluev <nepomiluev.kirill@gmail.com>
2021-10-15 02:09:51 +0300

    Урок 27

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Lessons());
 
-/// Сегодня мы познакомимся с созданием и оформлением всплывающего меню.
+/// На предыдущих уроках мы использовали только StatelessWidget(). У него был
+/// существенный минус: при помощи него невозможно было создать никаких
+/// динамических элементов, так как он не способен изменяться в процессе
+/// работы приложения. Для того, чтобы создавать динамические элементы нам
+/// понадобится новый тип виджета - StatefulWidget(). Его ключевое отличие
+/// заключается в следующем: при работе со StatelessWidget() метод build,
+/// который отвечает за отрисовку экрана приложения, срабатывал всего один
+/// раз - при запуске приложения. При работе со StatefulWidget() у нас
+/// появляется возможность многократно обращаться к методу build через
+/// метод setState. При срабатывании метода setState, срабатывает метод
+/// build, и экран перерисовывается.
 
-class Lessons extends StatelessWidget {
+class Lessons extends StatefulWidget {
+
+  @override
+  _LessonsState createState() => _LessonsState();
+}
+
+class _LessonsState extends State<Lessons> {
+  bool isClicked = false; /// Создадим переменную типа bool.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Уроки Flutter",
       home: Scaffold(
-
-        drawer: Drawer( /// Виджет Drawer() позволяет нам создать боковое меню
-          /// страницы
-
+        drawer: Drawer(
           child: Container(
             height: double.infinity,
             width: double.infinity,
@@ -126,7 +141,7 @@ class Lessons extends StatelessWidget {
                       children: [
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 5),
-                          child: Icon(Icons.timer, size: 30, color: Colors.lightBlueAccent,),
+                          child: Icon(Icons.timer_outlined, size: 30, color: Colors.lightBlueAccent,),
                         ),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 5),
@@ -185,17 +200,71 @@ class Lessons extends StatelessWidget {
         appBar: AppBar(
           centerTitle: true,
           title: Text(
-            "Урок 26",
+            "Органайзер",
           ),
           backgroundColor: Colors.blueAccent,
         ),
         backgroundColor: Colors.white,
+
+        floatingActionButton: FloatingActionButton( /// FloatingActionButton() -
+          /// одна из нескольких типов кнопок в Flutter. Подробнее на свойствах
+          /// и типах кнопок речь пойдет в следующих уроках, сейчас же, для
+          /// демонстрации принципа действия StatefulWidget() мы воспользуемся
+          /// лишь одним свойством - свойством onPressed, который отвечает за
+          /// действие кнопки.
+
+          onPressed: () {setState(() { /// Нажатие на кнопку будет вызывать
+            /// метод setState, который будет вызывать метод build, и экран
+            /// будет перестраиваться. При этом, метод setState будет менять
+            /// значение нашей переменной isClicked на противоположное.
+
+            isClicked = !isClicked;
+
+          });},
+
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(icon: Icon(Icons.account_box), label: "Аккаунт"),
             BottomNavigationBarItem(icon: Icon(Icons.edit), label: "Редактор"),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: "Поиск"),
           ],
+        ),
+        body: Center(
+          child: Container(
+            child: Center(
+              /// На данном этапе нам и понадобится созданная ранне переменная:
+              /// в зависимости от её значения, метод build будет проводить
+              /// различные построения. В нашем случае, эта переменная будет
+              /// менять текст внутри контейнера. Добьемся мы этого при помощи
+              /// тернарной операции:
+
+              child: isClicked? Text( // При isClicked = true.
+                "   Переменная\nisClicked = true",
+                style: TextStyle(fontSize: 30, color: Colors.white),)
+                  : Text( // При isClicker = false.
+                "   Переменная\nisClicked = false",
+                style: TextStyle(fontSize: 30, color: Colors.lightBlue),),
+
+            ),
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 4,
+                color: Colors.lightBlue,
+              ),
+
+              /// Введем еще одну небольную функцию: пусть при разных значениях
+              /// isClicked контейнер окрашивается в разные цвета.
+
+              color: isClicked? Colors.lightBlue  // При isClicked = true.
+                  : Colors.white70, // При isClicker = false.
+
+              shape: BoxShape.circle,
+            ),
+          ),
         ),
       ),
     );
