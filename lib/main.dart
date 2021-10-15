1f5dd62de2b0dbf1d9443770438a5dbde1b2cc5b
Kirill Nepomiluev <nepomiluev.kirill@gmail.com>
2021-10-15 04:41:18 +0300

    Урок 28

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Lessons());
 
-/// На предыдущих уроках мы использовали только StatelessWidget(). У него был
-/// существенный минус: при помощи него невозможно было создать никаких
-/// динамических элементов, так как он не способен изменяться в процессе
-/// работы приложения. Для того, чтобы создавать динамические элементы нам
-/// понадобится новый тип виджета - StatefulWidget(). Его ключевое отличие
-/// заключается в следующем: при работе со StatelessWidget() метод build,
-/// который отвечает за отрисовку экрана приложения, срабатывал всего один
-/// раз - при запуске приложения. При работе со StatefulWidget() у нас
-/// появляется возможность многократно обращаться к методу build через
-/// метод setState. При срабатывании метода setState, срабатывает метод
-/// build, и экран перерисовывается.
-
 class Lessons extends StatefulWidget {
-
   @override
   _LessonsState createState() => _LessonsState();
 }
 
 class _LessonsState extends State<Lessons> {
-  bool isClicked = false; /// Создадим переменную типа bool.
+  int x = 0;
+  int y = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -34,11 +22,10 @@ class _LessonsState extends State<Lessons> {
             width: double.infinity,
             decoration: BoxDecoration(
               border: Border(
-                right: BorderSide(
-                  color: Colors.blueAccent,
-                  width: 2,
-                )
-              ),
+                  right: BorderSide(
+                color: Colors.blueAccent,
+                width: 2,
+              )),
               gradient: LinearGradient(
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
@@ -62,10 +49,7 @@ class _LessonsState extends State<Lessons> {
                   child: Container(
                     height: 50,
                     decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 2,
-                        color: Colors.blueAccent
-                      ),
+                      border: Border.all(width: 2, color: Colors.blueAccent),
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(10),
                     ),
@@ -73,18 +57,21 @@ class _LessonsState extends State<Lessons> {
                       children: [
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 5),
-                          child: Icon(Icons.account_box, size: 30, color: Colors.lightBlueAccent,),
+                          child: Icon(
+                            Icons.account_box,
+                            size: 30,
+                            color: Colors.lightBlueAccent,
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 5),
                           child: Text(
                             "Профиль",
                             style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              fontStyle: FontStyle.italic,
-                              color: Colors.lightBlueAccent
-                            ),
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.lightBlueAccent),
                           ),
                         ),
                       ],
@@ -96,10 +83,7 @@ class _LessonsState extends State<Lessons> {
                   child: Container(
                     height: 50,
                     decoration: BoxDecoration(
-                      border: Border.all(
-                          width: 2,
-                          color: Colors.blueAccent
-                      ),
+                      border: Border.all(width: 2, color: Colors.blueAccent),
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(10),
                     ),
@@ -107,7 +91,11 @@ class _LessonsState extends State<Lessons> {
                       children: [
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 5),
-                          child: Icon(Icons.edit, size: 30, color: Colors.lightBlueAccent,),
+                          child: Icon(
+                            Icons.edit,
+                            size: 30,
+                            color: Colors.lightBlueAccent,
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 5),
@@ -117,8 +105,7 @@ class _LessonsState extends State<Lessons> {
                                 fontSize: 30,
                                 fontWeight: FontWeight.bold,
                                 fontStyle: FontStyle.italic,
-                                color: Colors.lightBlueAccent
-                            ),
+                                color: Colors.lightBlueAccent),
                           ),
                         ),
                       ],
@@ -130,10 +117,7 @@ class _LessonsState extends State<Lessons> {
                   child: Container(
                     height: 50,
                     decoration: BoxDecoration(
-                      border: Border.all(
-                          width: 2,
-                          color: Colors.blueAccent
-                      ),
+                      border: Border.all(width: 2, color: Colors.blueAccent),
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(10),
                     ),
@@ -141,7 +125,11 @@ class _LessonsState extends State<Lessons> {
                       children: [
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 5),
-                          child: Icon(Icons.timer_outlined, size: 30, color: Colors.lightBlueAccent,),
+                          child: Icon(
+                            Icons.timer_outlined,
+                            size: 30,
+                            color: Colors.lightBlueAccent,
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 5),
@@ -151,8 +139,7 @@ class _LessonsState extends State<Lessons> {
                                 fontSize: 30,
                                 fontWeight: FontWeight.bold,
                                 fontStyle: FontStyle.italic,
-                                color: Colors.lightBlueAccent
-                            ),
+                                color: Colors.lightBlueAccent),
                           ),
                         ),
                       ],
@@ -164,10 +151,7 @@ class _LessonsState extends State<Lessons> {
                   child: Container(
                     height: 50,
                     decoration: BoxDecoration(
-                      border: Border.all(
-                          width: 2,
-                          color: Colors.blueAccent
-                      ),
+                      border: Border.all(width: 2, color: Colors.blueAccent),
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(10),
                     ),
@@ -175,7 +159,11 @@ class _LessonsState extends State<Lessons> {
                       children: [
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 5),
-                          child: Icon(Icons.settings, size: 30, color: Colors.lightBlueAccent,),
+                          child: Icon(
+                            Icons.settings,
+                            size: 30,
+                            color: Colors.lightBlueAccent,
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 5),
@@ -185,8 +173,7 @@ class _LessonsState extends State<Lessons> {
                                 fontSize: 30,
                                 fontWeight: FontWeight.bold,
                                 fontStyle: FontStyle.italic,
-                                color: Colors.lightBlueAccent
-                            ),
+                                color: Colors.lightBlueAccent),
                           ),
                         ),
                       ],
@@ -205,65 +192,203 @@ class _LessonsState extends State<Lessons> {
           backgroundColor: Colors.blueAccent,
         ),
         backgroundColor: Colors.white,
-
-        floatingActionButton: FloatingActionButton( /// FloatingActionButton() -
-          /// одна из нескольких типов кнопок в Flutter. Подробнее на свойствах
-          /// и типах кнопок речь пойдет в следующих уроках, сейчас же, для
-          /// демонстрации принципа действия StatefulWidget() мы воспользуемся
-          /// лишь одним свойством - свойством onPressed, который отвечает за
-          /// действие кнопки.
-
-          onPressed: () {setState(() { /// Нажатие на кнопку будет вызывать
-            /// метод setState, который будет вызывать метод build, и экран
-            /// будет перестраиваться. При этом, метод setState будет менять
-            /// значение нашей переменной isClicked на противоположное.
-
-            isClicked = !isClicked;
-
-          });},
-
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         bottomNavigationBar: BottomNavigationBar(
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.account_box), label: "Аккаунт"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_box), label: "Аккаунт"),
             BottomNavigationBarItem(icon: Icon(Icons.edit), label: "Редактор"),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: "Поиск"),
           ],
         ),
         body: Center(
-          child: Container(
-            child: Center(
-              /// На данном этапе нам и понадобится созданная ранне переменная:
-              /// в зависимости от её значения, метод build будет проводить
-              /// различные построения. В нашем случае, эта переменная будет
-              /// менять текст внутри контейнера. Добьемся мы этого при помощи
-              /// тернарной операции:
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: FloatingActionButton(
+                  /// С данной кнопкой мы уже имели
+                  /// дело, так как она входит в одно из полей виджета
+                  /// Scaffold(). Рассмотри подробнее её свойства.
 
-              child: isClicked? Text( // При isClicked = true.
-                "   Переменная\nisClicked = true",
-                style: TextStyle(fontSize: 30, color: Colors.white),)
-                  : Text( // При isClicker = false.
-                "   Переменная\nisClicked = false",
-                style: TextStyle(fontSize: 30, color: Colors.lightBlue),),
+                  elevation: 20,
 
-            ),
-            width: 250,
-            height: 250,
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 4,
-                color: Colors.lightBlue,
+                  /// Свойство, меняющее уровень подъема кнопки
+                  /// над плоскостью экрана.
+
+                  backgroundColor: Colors.blue,
+
+                  /// Свойство, отвечающее
+                  /// за цвет самой кнопки.
+
+                  foregroundColor: Colors.white,
+
+                  /// Свойство, отвечающее за
+                  /// цвет дочернего элемента кнопки.
+
+                  splashColor: Colors.blueAccent,
+
+                  /// Свойство, отвечающее за
+                  /// цвет всплеска кнопки при нажатии.
+
+                  onPressed: () {
+                    setState(() {
+                      /// Свойство, описывающее
+                      /// действие, совершаемое при нажатии кнопки.
+
+                      x = x + 1;
+                    });
+                  },
+
+                  child: Center(
+                    /// Свойство, содержащее дочерний элемент
+                    /// кнопки.
+
+                    child: Icon(
+                      Icons.add,
+                      size: 40,
+                    ),
+                  ),
+                ),
               ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: SizedBox(
+                  width: 220,
+                  height: 70,
+                  child: ElevatedButton(
+                    /// Следующий тип кнопки - это
+                    /// ElevatedButton(). Рассмотрим его подробнее:
 
-              /// Введем еще одну небольную функцию: пусть при разных значениях
-              /// isClicked контейнер окрашивается в разные цвета.
+                    onPressed: () {
+                      setState(() {
+                        /// В данном типе кнопок
+                        /// метод onPressed реагирует только на коротковременные
+                        /// нажатия.
 
-              color: isClicked? Colors.lightBlue  // При isClicked = true.
-                  : Colors.white70, // При isClicker = false.
+                        x = x + 1;
+                      });
+                    },
 
-              shape: BoxShape.circle,
-            ),
+                    onLongPress: () {
+                      setState(() {
+                        /// У данного типа кнопок
+                        /// присутствует еще один тип нажатия - долгое нажатие.
+                        /// Зададим функцию и на этот тип нажатия:
+
+                        y = y + 1;
+                      });
+                    },
+
+                    style: ButtonStyle(
+                      /// Свойство, отвечающее за стиль текста.
+
+                      side: MaterialStateProperty.all(
+                          BorderSide(width: 2, color: Colors.black)),
+
+                      /// Свойство, отвечающее за рамку кнопки. Важно: большая
+                      /// часть уже знакомых раннее виджетов помещается.
+
+                      elevation: MaterialStateProperty.all(20),
+                      backgroundColor: MaterialStateProperty.all(Colors.red),
+                      foregroundColor: MaterialStateProperty.all(Colors.black),
+                      overlayColor: MaterialStateProperty.all(Colors.redAccent),
+
+                      /// Свойство, отвечающее за цвет всплеска кнопки при
+                      /// нажатии.
+
+                      textStyle: MaterialStateProperty.all(TextStyle(
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                        fontSize: 15,
+                      )),
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Icon(Icons.shopping_basket),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text("Добавить в корзину"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: IconButton(
+
+                    /// Третий тип кнопок - IconButton().
+
+                    iconSize: 40,
+
+                    /// Свойство, отвечающее за размер иконки.
+
+                    color: Colors.deepPurple,
+                    splashRadius: 30,
+                    // Свойство, отвечающее за радиус всплеска,
+                    // появляющегося при нажатии на кнопку.
+
+                    splashColor: Colors.purpleAccent,
+                    onPressed: () {
+                      setState(() {
+                        x = x + 1;
+                      });
+                    },
+                    icon: Icon(
+                        Icons.timer_outlined) // Свойство, содержащее в себе
+                    // иконку
+                    ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextButton(
+                  // Последний тип кнопок - TextButton()
+                  onPressed: () {
+                    setState(() {
+                      x = x + 1;
+                    });
+                  },
+                  onLongPress: () {
+                    setState(() {
+                      y = y + 1;
+                    });
+                  },
+                  child: Text("Новая заметка"), // Свойство, содержащее в себе
+                  // текст кнопки
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all(Colors.teal),
+                    textStyle: MaterialStateProperty.all(TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                    )),
+                  ),
+                ),
+              ),
+              Container(
+                width: 300,
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Colors.white70,
+                  border: Border.all(width: 2, color: Colors.black),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                    child: Text(
+                  "Число нажатий: $x\nЧисло долгих нажатий: $y",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                )),
+              )
+            ],
           ),
         ),
       ),
