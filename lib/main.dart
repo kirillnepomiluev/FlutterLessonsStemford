70d362cc5afc1c091697b6d48f0390cc4bc52af1
DagonSticks <dagonsticks.ineitp@gmail.com>
2021-10-17 10:32:39 +0300

    35 урок, измененный

--- lib/main.dart
+++ lib/main.dart
@@ -1,202 +1,107 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'dart:async';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> with TickerProviderStateMixin {
- late  AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: Duration(milliseconds: 2000),
-      vsync: this,
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  Future _startAnimation() async {
-    try {
-      await _controller.forward().orCancel;
-      await _controller.reverse().orCancel;
-    } on TickerCanceled {
-      print('Animation Failed');
-    }
-  }
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    timeDilation = 10.0;
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Animations'),
-        ),
-        body: GestureDetector(
-          onTap: () {
-            _startAnimation();
-          },
-          child: Center(
-            child: Container(
-              width: 350.0,
-              height: 350.0,
-              decoration: BoxDecoration(
-                color: Colors.grey.withOpacity(0.1),
-                border: Border.all(
-                  color: Colors.blueGrey.withOpacity(0.8),
-                ),
-              ),
-              child: AnimatedBox(
-                controller: _controller,
-              ),
+      home: MyAnimation(),
+    );
+  }
+}
+
+class MyAnimation extends StatefulWidget {
+
+  @override
+  _MyAnimationState createState() {
+    return _MyAnimationState();
+  }
+}
+
+class _MyAnimationState extends State<MyAnimation> {
+
+  bool isView = false;
+  double position = 40;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    // return Scaffold(
+    //   appBar: AppBar(title: Text('Animation example')),
+    //   body: Center(
+    //     child: AnimatedOpacity(
+    //       duration: Duration(milliseconds: 2000),
+    //       opacity: isView ? 0.0 : 1.0,
+    //       child: InkWell(
+    //         onTap: () => setState(() => isView = !isView),
+    //         child: Container(
+    //           width: 200.0,
+    //           height: 200.0,
+    //           color: Colors.green,
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
+
+    // return Scaffold(
+    //   appBar: AppBar(title: Text('Animation example')),
+    //   body: Center(
+    //     child: AnimatedOpacity(
+    //       duration: Duration(milliseconds: 400),
+    //       opacity: isView ? 0.0 : 1.0,
+    //       curve: ExampleCurve(),
+    //       child: GestureDetector(
+    //         onTap: () => setState(() => isView = !isView),
+    //         child: Container(
+    //           width: 200.0,
+    //           height: 200.0,
+    //           color: Colors.green,
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Animation example')),
+      body: Stack(
+        alignment: Alignment.centerLeft,
+        children: [
+          AnimatedPositioned(
+            left: position,
+            duration: Duration(milliseconds: 400),
+            child: Image.network('https://cdn.pixabay.com/photo/2012/04/11/17/34/car-29078_640.png',
+              width: 200.0,
             ),
           ),
-        ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('GO!'),
+        onPressed: () {
+          setState(() {
+            position += 40;
+          });
+        },
       ),
     );
   }
 }
 
-class AnimatedBox extends StatelessWidget {
-  AnimatedBox({Key? key, required this.controller})
-      : opacity = Tween<double>(
-    begin: 0.0,
-    end: 1.0,
-  ).animate(
-    CurvedAnimation(
-      parent: controller,
-      curve: Interval(
-        0.0,
-        0.2,
-        curve: Curves.fastOutSlowIn,
-      ),
-    ),
-  ),
-        radius = BorderRadiusTween(
-          begin: BorderRadius.circular(0.0),
-          end: BorderRadius.circular(100.0),
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.5,
-              0.75,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
 
-        rotate = Tween<double>(
-          begin: 0.0,
-          end: 3.141 * 4,
-        ).animate(CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.1,
-              0.3,
-              curve: Curves.ease,
-            ))),
+class ExampleCurve extends Curve {
+  final double count;
 
-        width = Tween<double>(
-          begin: 50.0,
-          end: 200.0,
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.3,
-              0.6,
-              curve: Curves.fastOutSlowIn,
-            ),
-          ),
-        ),
-        height = Tween<double>(
-          begin: 50.0,
-          end: 200.0,
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.4,
-              0.6,
-              curve: Curves.fastOutSlowIn,
-            ),
-          ),
-        ),
-
-        color = ColorTween(
-          begin: Colors.red[200],
-          end: Colors.deepPurple[900],
-        ).animate(CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.0,
-              0.75,
-              curve: Curves.linear,
-            ))),
-
-        movement = EdgeInsetsTween(
-          begin: EdgeInsets.only(bottom: 10.0, left: 0.0),
-          end: EdgeInsets.only(bottom: 100.0, left: 75.0),
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.2,
-              0.4,
-              curve: Curves.fastOutSlowIn,
-            ),
-          ),
-        ),
-        super(key: key);
-
-  final Animation<double> controller;
-  final Animation<double> opacity;
-  final Animation<double> width;
-  final Animation<double> height;
-  final Animation<EdgeInsets> movement;
-  final Animation<BorderRadius> radius;
-  final Animation<Color?> color;
-  final Animation<double> rotate;
+  ExampleCurve({this.count = 3});
 
   @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (BuildContext context, Widget? child) {
-        return new Container(
-          padding: movement.value,
-          transform: Matrix4.identity()..rotateZ(rotate.value),
-          alignment: Alignment.center,
-          child: Opacity(
-            opacity: opacity.value,
-            child: Container(
-              width: width.value,
-              height: height.value,
-              decoration: BoxDecoration(
-                color: color.value,
-                border: Border.all(
-                  color: Colors.cyan,
-                  width: 2.0,
-                ),
-                borderRadius: radius.value,
-              ),
-            ),
-          ),
-        );
-      },
-    );
+  double transformInternal(double t) {
+    var val = sin(count * 2 * pi * t) * 0.5 + 0.5;
+    return val;
   }
 }
