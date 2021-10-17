05bfa2e3d177d3d41f2d25bd26285207d004836a
DagonSticks <dagonsticks.ineitp@gmail.com>
2021-10-17 15:16:04 +0300

    Урок 37

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -14,61 +13,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class MyAnimation extends StatefulWidget {
-//
-//   @override
-//   _MyAnimationState createState() {
-//     return _MyAnimationState();
-//   }
-// }
-//
-// class _MyAnimationState extends State<MyAnimation> {
-//
-//   bool isChangeWidget = false;
-//   bool startAnimation = false;
-//
-//   @override
-//   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   appBar: AppBar(title: Text('Animation example')),
-    //   body: Center(
-    //     child: SizedBox(
-    //       width: 200.0,
-    //       height: 200.0,
-    //       child: InkWell(
-    //         onTap: () => setState(() => isChangeWidget = !isChangeWidget),
-    //         child: AnimatedCrossFade(
-    //           crossFadeState: isChangeWidget ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-    //           duration: const Duration(seconds: 2),
-    //           firstChild: Container(color: Colors.green),
-    //           secondChild: Container(color: Colors.red),
-    //         ),
-    //       ),
-    //     ),
-    //   ),
-    // );
-
-    // return Scaffold(
-    //   appBar: AppBar(title: Text('Animation example')),
-    //   body: Center(
-    //     child: InkWell(
-    //       onTap: () => setState(() => startAnimation = !startAnimation),
-    //       child: AnimatedContainer(
-    //         decoration: BoxDecoration(
-    //           color: startAnimation ? Colors.lightGreen : Colors.red,
-    //           borderRadius: BorderRadius.circular(startAnimation ? 15.0 : 0.0),
-    //         ),
-    //         width: startAnimation ? 100 : 200,
-    //         height: startAnimation ? 100 : 200,
-    //         curve: Curves.easeInOutCubic,
-    //         duration: Duration(seconds: 1),
-    //       ),
-    //     ),
-    //   ),
-    // );
-//   }
-// }
-
 class MyApp2 extends StatefulWidget {
 
   @override
@@ -77,30 +21,55 @@ class MyApp2 extends StatefulWidget {
   }
 }
 
-
-class _MyApp2State extends State<MyApp2> with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-
-  @override
-  void initState() {
-    _animationController = AnimationController(duration: Duration(milliseconds: 1000), vsync: this);
-    _animationController.addListener(() => setState(() {}));
-    super.initState();
-  }
+class _MyApp2State extends State<MyApp2>{
+  String value = "Сохранение";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Animation example')),
-      body: InkWell(
-        onTap: () => _animationController.forward(),
-        child: Opacity(
-          opacity: 1.0 - _animationController.value,
-          child: Container(
-            color: Colors.orange,
-            height: 200.0,
-            width: 200.0 + (200 * _animationController.value),
-          ),
+      appBar: AppBar(
+        title: Text("Сохранение и загрузка данных"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+                child: Text("")
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                  onPressed: () async {
+                    SharedPreferences prefs = await SharedPreferences.getInstance();
+                    prefs.setString("TextString_key", "Сохранение 1");
+                  },
+                  child: Text("Сохранить игру"),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () async {
+                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  setState(() {
+                    value = prefs.setString("TextString_key") ?? "NULL";
+                  });
+                },
+                child: Text("Сохранить игру"),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () async {
+                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  prefs.remove("TextString_key");
+                },,
+                child: Text("Сохранить игру"),
+              ),
+            ),
+          ],
         ),
       ),
     );
