3240195b010cebb854ef8e9a46c9cd5e61b1ec3b
kirillnepomiluev <yallghgh789>
2021-10-19 17:35:43 +0300

    после урока

--- lib/main.dart
+++ lib/main.dart
@@ -24,38 +24,21 @@ class MyAnimation extends StatefulWidget {
 class _MyAnimationState extends State<MyAnimation> {
 
   bool isView = false;
-  double position = 40;
+  double position = 10;
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(title: const Text('Урок по анимации часть 1')),
-      body: Center(
-        child: AnimatedOpacity(
-          curve: Curves.bounceInOut,
-          duration: const Duration(milliseconds: 2000),
-          opacity: isView ? 0.0 : 1.0,
-          child: InkWell(
-            onTap: () => setState(() => isView = !isView),
-            child: Container(
-              width: 200.0,
-              height: 200.0,
-              color: Colors.green,
-            ),
-          ),
-        ),
-      ),
-    );
-
     // return Scaffold(
-    //   appBar: AppBar(title: Text('Animation example')),
+    //   appBar: AppBar(title: const Text('Урок по анимации часть 1')),
     //   body: Center(
     //     child: AnimatedOpacity(
-    //       duration: Duration(milliseconds: 400),
-    //       opacity: isView ? 0.0 : 1.0,
     //       curve: ExampleCurve(),
-    //       child: GestureDetector(
+    //       //кривая по которой просиходит расчет
+    //       // значений для анимации
+    //       duration: const Duration(milliseconds: 2000),
+    //       opacity: isView ? 0.0 : 1.0,
+    //       child: InkWell(
     //         onTap: () => setState(() => isView = !isView),
     //         child: Container(
     //           width: 200.0,
@@ -67,32 +50,51 @@ class _MyAnimationState extends State<MyAnimation> {
     //   ),
     // );
 
-  //   return Scaffold(
-  //     appBar: AppBar(title: Text('Animation example')),
-  //     body: Stack(
-  //       alignment: Alignment.centerLeft,
-  //       children: [
-  //         AnimatedPositioned(
-  //           left: position,
-  //           duration: const Duration(milliseconds: 400),
-  //           child: Image.network('https://cdn.pixabay.com/photo/2012/04/11/17/34/car-29078_640.png',
-  //             width: 200.0,
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //     floatingActionButton: FloatingActionButton(
-  //       child: const Text('GO!'),
-  //       onPressed: () {
-  //         setState(() {
-  //           position += 40;
-  //         });
-  //       },
-  //     ),
-  //   );
-  // }
+
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Animation example')),
+      body: Stack(
+        alignment: Alignment.centerLeft,
+        children: [
+          AnimatedPositioned(
+            curve: Curves.ease,
+            left: position,
+            duration: const Duration(milliseconds: 600),
+            child: Image.network('https://cdn.pixabay.com/photo/2012/04/11/17/34/car-29078_640.png',
+              width: 200.0,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: FloatingActionButton(
+              child: const Text('Start'),
+              onPressed: () {
+                setState(() {
+                  position = 10;
+                });
+              },
+            ),
+          ),
+          FloatingActionButton(
+            child: const Text('GO!'),
+            onPressed: () {
+              setState(() {
+                position += 350;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
  }
-}
+
 
 
 class ExampleCurve extends Curve {
