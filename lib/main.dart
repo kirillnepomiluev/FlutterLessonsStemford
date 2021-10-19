3ec8486193d3206195fdaadfff4a2b3b22b6e8f2
Kirill Nepomiluev <nepomiluev.kirill@gmail.com>
2021-10-19 14:58:43 +0300

    Урок 33 исправленный

--- lib/main.dart
+++ lib/main.dart
@@ -1,202 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'dart:async';
+import 'package:lessons/classList.dart';
+import 'package:lessons/router.dart';
+import 'package:lessons/secondScreen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> with TickerProviderStateMixin {
- late  AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: Duration(milliseconds: 2000),
-      vsync: this,
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  Future _startAnimation() async {
-    try {
-      await _controller.forward().orCancel;
-      await _controller.reverse().orCancel;
-    } on TickerCanceled {
-      print('Animation Failed');
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    timeDilation = 10.0;
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Animations'),
-        ),
-        body: GestureDetector(
-          onTap: () {
-            _startAnimation();
-          },
-          child: Center(
-            child: Container(
-              width: 350.0,
-              height: 350.0,
-              decoration: BoxDecoration(
-                color: Colors.grey.withOpacity(0.1),
-                border: Border.all(
-                  color: Colors.blueGrey.withOpacity(0.8),
-                ),
-              ),
-              child: AnimatedBox(
-                controller: _controller,
-              ),
-            ),
-          ),
-        ),
-      ),
+      title: "Уроки Flutter",
+      home: Lessons(),
+      onGenerateRoute: MyRouter.generateRoute,
+      initialRoute: RouteNames.main,
     );
   }
 }
 
-class AnimatedBox extends StatelessWidget {
-  AnimatedBox({Key? key, required this.controller})
-      : opacity = Tween<double>(
-    begin: 0.0,
-    end: 1.0,
-  ).animate(
-    CurvedAnimation(
-      parent: controller,
-      curve: Interval(
-        0.0,
-        0.2,
-        curve: Curves.fastOutSlowIn,
-      ),
-    ),
-  ),
-        radius = BorderRadiusTween(
-          begin: BorderRadius.circular(0.0),
-          end: BorderRadius.circular(100.0),
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.5,
-              0.75,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
+class Lessons extends StatefulWidget {
+  @override
+  _LessonsState createState() => _LessonsState();
+}
 
-        rotate = Tween<double>(
-          begin: 0.0,
-          end: 3.141 * 4,
-        ).animate(CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.1,
-              0.3,
-              curve: Curves.ease,
-            ))),
-
-        width = Tween<double>(
-          begin: 50.0,
-          end: 200.0,
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.3,
-              0.6,
-              curve: Curves.fastOutSlowIn,
-            ),
-          ),
-        ),
-        height = Tween<double>(
-          begin: 50.0,
-          end: 200.0,
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.4,
-              0.6,
-              curve: Curves.fastOutSlowIn,
-            ),
-          ),
-        ),
-
-        color = ColorTween(
-          begin: Colors.red[200],
-          end: Colors.deepPurple[900],
-        ).animate(CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.0,
-              0.75,
-              curve: Curves.linear,
-            ))),
-
-        movement = EdgeInsetsTween(
-          begin: EdgeInsets.only(bottom: 10.0, left: 0.0),
-          end: EdgeInsets.only(bottom: 100.0, left: 75.0),
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.2,
-              0.4,
-              curve: Curves.fastOutSlowIn,
-            ),
-          ),
-        ),
-        super(key: key);
-
-  final Animation<double> controller;
-  final Animation<double> opacity;
-  final Animation<double> width;
-  final Animation<double> height;
-  final Animation<EdgeInsets> movement;
-  final Animation<BorderRadius> radius;
-  final Animation<Color?> color;
-  final Animation<double> rotate;
+class _LessonsState extends State<Lessons> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (BuildContext context, Widget? child) {
-        return new Container(
-          padding: movement.value,
-          transform: Matrix4.identity()..rotateZ(rotate.value),
-          alignment: Alignment.center,
-          child: Opacity(
-            opacity: opacity.value,
-            child: Container(
-              width: width.value,
-              height: height.value,
-              decoration: BoxDecoration(
-                color: color.value,
-                border: Border.all(
-                  color: Colors.cyan,
-                  width: 2.0,
-                ),
-                borderRadius: radius.value,
-              ),
-            ),
-          ),
-        );
-      },
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Страница 1"),
+      ),
+     body: Center(
+       child: IconButton(
+         onPressed: (){
+           Navigator.push(context,
+               MaterialPageRoute(builder: (context) => Lessons2()));
+
+         //  Navigator.pushNamed(context, RouteNames.secondPage);
+         },
+         icon: Icon(Icons.arrow_forward),
+       ),
+     ),
     );
   }
 }
