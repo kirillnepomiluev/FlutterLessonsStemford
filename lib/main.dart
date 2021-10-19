156410c3f7f43d050ea6427091877b9fc4340b77
kirillnepomiluev <yallghgh789>
2021-10-19 16:54:53 +0300

    изменения рефактор

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ import 'package:lessons/secondScreen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Уроки Flutter",
       home: Lessons(),
       onGenerateRoute: MyRouter.generateRoute,
@@ -19,18 +21,31 @@ class MyApp extends StatelessWidget {
 }
 
 class Lessons extends StatefulWidget {
+  const Lessons({Key? key}) : super(key: key);
+
   @override
   _LessonsState createState() => _LessonsState();
 }
 
 class _LessonsState extends State<Lessons> {
 
+
+  @override
+  void initState() {
+    AssetsAudioPlayer.newPlayer().open(
+      Audio("assets/audios/song.mp3"),
+      autoStart: true,
+      showNotification: true,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Страница 1"),
+        title: const Text("Страница 1"),
       ),
      body: Center(
        child: IconButton(
@@ -41,7 +56,7 @@ class _LessonsState extends State<Lessons> {
              showNotification: true,
            );
          },
-         icon: Icon(Icons.play_circle_fill),
+         icon: const Icon(Icons.play_circle_fill, size: 70,),
        ),
      ),
     );
